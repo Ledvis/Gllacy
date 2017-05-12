1ef6d8d54fec16c5843c78244ff13b3b85cd3c53
Eugene <fedyai.eugen@gmail.com>
2017-05-12 18:22:23 +0300

    v 3.0.1

--- script/script-2.js
+++ script/script-2.js
@@ -1,7 +1,3 @@
-var body = document.querySelector("body");
-var green = document.querySelector(".green-toggle");
-var purple = document.querySelector(".purple-toggle");
-var brown = document.querySelector(".brown-toggle");
 var catalogLink = document.querySelector(".catalog-menu");
 var catalogPopup = document.querySelector(".catalog-popup");
 var searchLink = document.querySelector(".user-search");
@@ -10,44 +6,7 @@ var enterLink = document.querySelector(".user-enter");
 var enterPopup = document.querySelector(".enter-popup");
 var cartLink = document.querySelector(".user-cart");
 var cartPopup = document.querySelector(".cart-popup");
-var link = document.querySelector(".link");
-var formPopup = document.querySelector(".form-popup");
-var formClose = formPopup.querySelector(".form-popup-close");
-var overlay = document.querySelector(".overlay");
-var login = formPopup.querySelector("[name=name]");
-var password = formPopup.querySelector("[name=email]");
-var form = formPopup.querySelector("form");
-var storage = localStorage.getItem("login");
 
-green.addEventListener("click", function (event) {
-	event.preventDefault();
-	body.classList.add("green-bg");
-	green.classList.add("active-toggle");
-	body.classList.remove("purple-bg");
-	body.classList.remove("brown-bg");
-	purple.classList.remove("active-toggle");
-	brown.classList.remove("active-toggle");
-});
-
-purple.addEventListener("click", function (event) {
-	event.preventDefault();
-	body.classList.add("purple-bg");
-	purple.classList.add("active-toggle");
-	body.classList.remove("green-bg");
-	body.classList.remove("brown-bg");
-	green.classList.remove("active-toggle");
-	brown.classList.remove("active-toggle");
-});
-
-brown.addEventListener("click", function (event) {
-	event.preventDefault();
-	body.classList.add("brown-bg");
-	brown.classList.add("active-toggle");
-	body.classList.remove("purple-bg");
-	body.classList.remove("green-bg");
-	purple.classList.remove("active-toggle");
-	green.classList.remove("active-toggle");
-});
 
 catalogLink.addEventListener("click", function (event) {
 	event.preventDefault();
@@ -113,41 +72,5 @@ cartLink.addEventListener("click", function (event) {
 		enterLink.classList.remove("active-item-popup");
 		searchPopup.classList.remove("search-popup-show");
 		searchLink.classList.remove("active-item-popup");
-	}
-});
-
-link.addEventListener("click", function (event) {
-	event.preventDefault();
-	formPopup.classList.add("form-popup-show");
-	overlay.classList.add("overlay-show");
-	if (storage) {
-		storage = login.value;
-		login.focus();
-	} else {
-		password.focus();
-	}
-});
-
-formClose.addEventListener("click", function (event) {
-	event.preventDefault();
-	formPopup.classList.remove("form-popup-show");
-	overlay.classList.remove("overlay-show");
-});
-
-window.addEventListener("keydown", function (event) {
-	if (event.keyCode === 27) {
-		if (formPopup.classList.contains("form-popup-show")) {
-			formPopup.classList.remove("form-popup-show");
-			overlay.classList.remove("overlay-show");
-		}
-	}
-});
-
-form.addEventListener("submit", function (event) {
-	if (!login.value || !password.value) {
-		event.preventDefault();
-		formPopup.classList.add("form-popup-error");
-	} else {
-		localStorage.setItem("login", login.value);
 	}
 });
